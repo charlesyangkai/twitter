35bb512492defcb15ccc68f83093c6120541ecb3
Charles Lee <charleslee94@hotmail.com>
2017-03-25 20:49:33 +0800

    twitter first commit :)

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Charles Lee on 24/3/17.
+//  Copyright © 2017 RebornAsNoob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
